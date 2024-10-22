2e2c3448cab162a676b543503d1dc9fcdc55fbb0
Morguen Caffrey <N00230651@student.iadt.ie>
2024-10-22 15:47:08 +0100

    displaying resource in controller

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -36,7 +36,7 @@ class AlbumController extends Controller
      */
     public function show(Album $album)
     {
-        //
+        return view('albums.show')->with('album', $album);
     }
 
     /**
