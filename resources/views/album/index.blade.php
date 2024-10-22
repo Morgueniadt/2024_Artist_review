0d5114c8aa9b509c11987488c70fb052679c72c3
Morguen Caffrey <N00230651@student.iadt.ie>
2024-10-22 15:39:18 +0100

    index code fixed

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -1,30 +1,26 @@
-
 <x-app-layout>
-<x-slot name="header">
-<h2 class="font-semibold text-x1 text-gray-800 leading-tight"> {{ _('All Albums') }}
-</h2> </x-slot>
-<div class="py-12">
-<div class="max-w-7x1 mx-auto sm: px-6 1g:px-8">
-<div class="bg-white overflow-hidden shadow-sm sm: rounded-lg"> <div class="p-6 text-gray-900">
-<h3 class="font-semibold text-lg mb-4">List of Albums:</h3>
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-@foreach ($albums as $album)
-<x-album-card
-:title="$album->title"
-:image="$album->image"
-:duration="$album->duration"
-:release_date="$album->release date"
-:number_of_songs="$album->number of songs"
-/>
-:title="$album->title"
-:image="$album->image"
-:duration="$album->duration"
-:release_date="$album->release date"
-:number_of_songs="$album->number of songs"
-@endforeach
-</div>
-</div>
-</div>
-</div>
-</div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ __('All Albums') }}</h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="font-semibold text-lg mb-4">List of Albums:</h3>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                        @foreach ($albums as $album)
+                            <x-album-card
+                                :title="$album->title"
+                                :image="$album->image"
+                                :duration="$album->duration"
+                                :release_date="$album->release_date"
+                                :number_of_songs="$album->number_of_songs"
+                            />
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
