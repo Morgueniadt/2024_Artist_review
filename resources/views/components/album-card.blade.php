08ec7ec858f3d3d3b5cdb2c4cde7baee6f4378b5
Morguen Caffrey <N00230651@student.iadt.ie>
2024-10-22 15:33:49 +0100

    album card edited

--- resources/views/components/album-card.blade.php
+++ resources/views/components/album-card.blade.php
@@ -1,3 +1,8 @@
 <div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+ @props(['title', 'duration ', 'release_date', 'number_of_songs'])
+<div class="border rounded-1g shadow-md p-6 bg-white hover: shadow-lg transition duration-300"> <h4 class="font-bold text-lg">{{ $title }}</h4>
+<img src="{{asset('images/books/' . $image)}}" alt="{{$title}}">
+<p class="text-gray-600">({{ $duration }})</p>
+<p class="text-gray-800 mt-4">{{ $release_date }}</p>
+<p class="text-gray-800 mt-4">{{ $number_of_songs }}</p>
 </div>
